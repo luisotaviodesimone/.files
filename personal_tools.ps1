774f3f275d3523f793017746c7cd9774f5a2bcfb
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-02-02 12:53:43 -0300

    Update personal_tools.ps1

--- personal_tools.ps1
+++ personal_tools.ps1
@@ -1,5 +1,5 @@
 Write-Output "The following personal tools will be installed"
-Write-Output chrome whatsapp notion discord slack ' '
+Write-Output chrome whatsapp notion discord slack "github desktop" authy ' '
 
 Read-Host -Prompt "Do you wish to continue? Yes(enter) No(ctrl+c)"
 
@@ -9,6 +9,7 @@ choco install notion -y
 choco install discord.install -y
 choco install slack -y
 choco install github-desktop -y
+choco install authy-desktop -y
 
 Write-Output "The following personal tools were installed"
-Write-Output chrome whatsapp notion discord slack "github desktop" ' '
+Write-Output chrome whatsapp notion discord slack "github desktop" authy ' '
