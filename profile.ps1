4c853148acffddf89178b75069e2b905be813efc
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-02-03 13:32:51 -0300

    add function to oh-my-posh themes

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,13 @@
 clear
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json | Invoke-Expression
 
+Enable-PoshTooltips
 
 # Set navigational functions
 function .. { set-location "../"}
 function ~ { set-location "~"}
 function profile { set-location $home/Documents/WindowsPowerShell}
+function themes { set-location ~\AppData\Local\Programs\oh-my-posh\themes}
 function wtSettings { set-location "~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"}
 
 # Shows navigable menu of all options when hitting Tab
