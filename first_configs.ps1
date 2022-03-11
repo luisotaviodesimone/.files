60d355c3eeb8db2d34944421f7ac04a7c95fd799
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-03-10 22:44:13 -0300

    add scoop package manager, test powershell curl alias absensce and neovim installation
    follow todo

--- first_configs.ps1
+++ first_configs.ps1
@@ -1,14 +1,20 @@
 # Installing chocolotay package manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Installing scoop package manager
+iwr -useb get.scoop.sh | iex
+
 # Installing the following choco packages
 choco install vscode -y
 choco install git.install -y
-choco install nvm -y
 choco install oh-my-posh -y
 
+# Installing the following scoop packages
+scoop install nvm
+scoop install neovim gcc
+
 # Adding my custom theme (similar to patricksvensson)
-cp my_custom_theme.omp.json  ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json
+cp my_custom_theme.omp.json ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json
 
 # Creating symbolic links for the .gitconfig and the powershell profile
 New-Item -ItemType SymbolicLink -Force -Path "~/.gitconfig" -Target ".gitconfig"
@@ -24,4 +30,14 @@ Read-Host -Prompt "Do you wish to continue? Yes(Enter) No(ctrl+c)"
 nvm install 14.19.0
 nvm use 14.19.0
 
+. $profile
+
 Write-Output "You now have nodejs installed on your machine"
+
+Write-Output "The following tools will be installed"
+Write-Output typescript ' '
+
+Read-Host -Prompt "Do you wish to continue? Yes(Enter) No(ctrl+c)"
+
+npm i -g typescript
+npm i -g yarn
--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,9 @@
 clear
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json | Invoke-Expression
 
+# Import/ Enable features
 Enable-PoshTooltips
+Import-Module Terminal-Icons
 
 # Set navigational functions
 function .. { set-location "../"}
@@ -10,11 +12,21 @@ function profile { set-location $home/Documents/WindowsPowerShell}
 function themes { set-location ~\AppData\Local\Programs\oh-my-posh\themes}
 function wtSettings { set-location "~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState"}
 
+# Removing alias (curl alias for Invoke-Web-Request)
+if (Test-Path alias:curl) {
+	Remove-Item alias:curl
+}
+
+# Setting new aliases
+Set-Alias curl curl.exe
+Set-Alias vim nvim
+
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 # Autocompletion for Arrow keys
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
