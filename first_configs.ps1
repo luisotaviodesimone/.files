5ed84d68d280296ea5a56e6b649d4956f76a7a7f
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-04-27 22:53:36 -0300

    finish first_configs.ps1

--- first_configs.ps1
+++ first_configs.ps1
@@ -1,8 +1,12 @@
-# Installing chocolotay package manager
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#Requires -RunAsAdministrator
 
-# Installing scoop package manager
-iwr -useb get.scoop.sh | iex
+# Installing chocolotay package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Installing scoop package manager in Administrator mode
+Invoke-WebRequest -useb get.scoop.sh -Outfile 'install.ps1'
+. .\install.ps1 -RunAsAdmin
+Remove-Item .\install.ps1
 
 Write-Output "The following tools will be installed"
 Write-Output vscode git github-cli oh-my-posh nvm neovim ' '
@@ -18,27 +22,32 @@ choco install vscode -y
 choco install oh-my-posh -y
 
 # Insert git clone here to guarantee below files existance
+$dotFilesPath = "$HOME\.files-test" # Name of your dotfiles directory
+
+git clone https://github.com/luisotaviodesimone/.files $dotFilesPath
 
 # Adding my custom theme (similar to patricksvensson)
-cp my_custom_theme.omp.json ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json
+Copy-Item $dotFilesPath\my_custom_theme.omp.json ~\AppData\Local\Programs\oh-my-posh\themes\CUSTOM_THEME.omp.json
 
-$major,$minor = $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor
-
-if ($major -eq 5 -and $minor -eq 1) {
-  # Creating symbolic links for the .gitconfig and the powershell profile
-  New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Target .\.gitconfig
-  New-Item -ItemType SymbolicLink -Force -Path $PROFILE -Target .\profile.ps1
-  }
+$major, $minor = $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor
+if ($major -ne 5 -and $minor -ne 1) {
+  # Re-launch as version 5 if we're not already
+  Write-Host "Now Running on PowerShell 5.1"
+  powershell -Version 5.1 Write-Host "Creating Symbolic Links";
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Target $dotFilesPath\.gitconfig ;
+  New-Item -ItemType SymbolicLink -Force -Path $PROFILE -Target $dotFilesPath\profile.ps1;
+  New-Item -ItemType SymbolicLink -Force -Path $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Target $dotFilesPath\profile.ps1
+}
 else {
-  Write-Host "`r`nThe Symbolink Links were not created"
-  }
+  New-Item -ItemType SymbolicLink -Force -Path $HOME/.gitconfig -Target $dotFilesPath\.gitconfig
+  New-Item -ItemType SymbolicLink -Force -Path $PROFILE -Target $dotFilesPath\profile.ps1
+  New-Item -ItemType SymbolicLink -Force -Path $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Target $dotFilesPath\profile.ps1
+}
 
 . $PROFILE
 
 Write-Output "The following tools will be installed"
 Write-Output node yarn ' '
-
-Read-Host -Prompt "Do you wish to continue? Yes(Enter) No(ctrl+c)"
 
 nvm install 14.19.0
 nvm use 14.19.0
@@ -50,7 +59,7 @@ Write-Output "You now have nodejs installed on your machine"
 Write-Output "The following tools will be installed"
 Write-Output typescript yarn
 
-Read-Host -Prompt "Do you wish to continue? Yes(Enter) No(ctrl+c)"
-
 npm i -g typescript
 npm i -g yarn
+
+exit
