338e03d7dfe3084fd7538887222708991b1d85de
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-04-30 18:24:39 -0300

    fix: history_backup_task

--- commandHistoryBackupTask/history_backup_task.ps1
+++ commandHistoryBackupTask/history_backup_task.ps1
@@ -1,6 +1,6 @@
 $dotFilesPath = "$HOME\.files\commandHistoryBackupTask"
 $consoleHistoryCopy = "$dotFilesPath\ConsoleHost_history.txt"
 
-. $dotFilesPath\create-unique-commands-history.ps1
+. $dotFilesPath\create_unique_commands_history.ps1
 gh gist edit 6995cd5f61538f67881e2bef26f00a8c $consoleHistoryCopy
 Remove-Item $consoleHistoryCopy 
