ef756630e9a4ee5d92315ebf17194a0669966b57
luisotaviodesimone <luisotaviodesimone@gmail.com>
2023-09-08 23:04:59 -0300

    feat: start linux .files development

--- commandHistoryBackupTask/create_unique_commands_history.ps1
+++
@@ -1,9 +0,0 @@
-$dotFilesPath = "$HOME\.files\commandHistoryBackupTask"
-
-Set-Variable -Name "TextFile" -Value $TERMINAL_HISTORY
-
-$text = Get-Content $TextFile -Raw
-
-$result = $text.Split("`r`n")
-
-$result | Sort-Object | Get-Unique | Out-File $dotFilesPath\ConsoleHost_history.txt
---
+++ installations.sh
@@ -0,0 +1,7 @@
+# sudo apt install golang
+# sudo apt install libasound2-dev
+# sudo apt install libudev-dev
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sudo apt install zsh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# sudo apt install gh
