7f5b037a7f0dd6c14d89753c81e1366a3aeebebd
luisotaviodesimone <luisotaviodesimone@gmail.com>
2023-10-03 13:46:51 -0300

    feat: organize main installation script

--- installations.sh
+++ installations.sh
@@ -1,24 +1,26 @@
-### Configure shell
+### Apt Installations
 sudo apt install neovim
 sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-. ./install-kitty.sh
+sudo apt install gh
+sudo apt install golang
+sudo apt install libasound2-dev # Necessary for some sound golang packages
+sudo apt install libudev-dev    # Necessary for some sound golang packages
 
-ln .zshrc ~/.zshrc
+### Install Oh-My-Zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ln .zshrc ~/.zshrc --force
 
-# Install oh-my-zsh plugins
+### Install oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
+### Install `nvm`
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-###
 
-### Install some dev tools
-sudo apt install gh
-sudo apt install golang
-sudo apt install libasound2-dev
-sudo apt install libudev-dev
+### Add modularized and personal installation scripts
 . ./install-vscode.sh
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo apt install google-chrome-stable_current_amd64.deb
+. ./install-chrome.sh
+. ./install-kitty.sh
+
