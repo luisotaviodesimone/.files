b9ac0107483a9cc058019d4bf5963ef942f65a2d
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-10-05 18:39:52 -0300

    refactor: organize

--- installations.sh
+++ installations.sh
@@ -5,20 +5,19 @@ export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
 ### Apt Installations
 sudo apt install neovim -y
 sudo apt install zsh -y
-
-sudo apt install ranger -y
-echo "set preview_images true" >> ~/.config/ranger/rc.conf
-
 sudo apt install gh -y
 sudo apt install golang -y
 sudo apt install libasound2-dev -y # Necessary for some sound golang packages
 sudo apt install libudev-dev -y    # Necessary for some sound golang packages
 
+sudo apt install ranger -y
+echo "set preview_images true" >> ~/.config/ranger/rc.conf
+
 ### Install `nvm`
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
 ### Add modularized and personal installation scripts
-. $DOT_FILES_DIR/install-oh-my-zsh.sh
-. $DOT_FILES_DIR/install-vscode.sh
-. $DOT_FILES_DIR/install-chrome.sh
-. $DOT_FILES_DIR/install-kitty.sh
+. $DOT_FILES_DIR/installation-scripts/install-oh-my-zsh.sh
+. $DOT_FILES_DIR/installation-scripts/install-vscode.sh
+. $DOT_FILES_DIR/installation-scripts/install-chrome.sh
+. $DOT_FILES_DIR/installation-scripts/install-kitty.sh
---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+
+ls $DOT_FILES_DIR
