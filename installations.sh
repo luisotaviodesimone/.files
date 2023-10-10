f1c54488b0e53e49e09bc58ec1c7669cff60063e
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-10-09 23:10:15 -0300

    feat: add my-go-cli to automatic installs

--- installations.sh
+++ installations.sh
@@ -19,6 +19,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 ### Add modularized installation scripts
 . $DOT_FILES_DIR/installation-scripts/install-oh-my-zsh.sh
 . $DOT_FILES_DIR/installation-scripts/install-kitty.sh
+. $DOT_FILES_DIR/installation-scripts/install-my-go-cli.sh
 
 ### Prompt for flatpak installations
 
