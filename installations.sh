a9f40d5cb910d1cc6d497df0bfbeef8f98ac9aa6
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-10-11 21:16:42 -0300

    feat: format and add httpie and wireguard

--- installations.sh
+++ installations.sh
@@ -5,35 +5,42 @@ export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
 YELLOW='\033[1;33m'
 RESET='\033[0;0m'
 
-### Apt Installations
-echo -e "$YELLOW Installing neovim...$RESET"
-sudo apt install neovim -y
-echo -e "$YELLOW Installing zsh...$RESET"
-sudo apt install zsh -y
-echo -e "$YELLOW Installing gh...$RESET"
-sudo apt install gh -y
-echo -e "$YELLOW Installing golang...$RESET"
-sudo apt install golang -y
-sudo apt install libasound2-dev -y # Necessary for some sound golang packages
-sudo apt install libudev-dev -y    # Necessary for some sound golang packages
+apt_apps=(
+    neovim
+    zsh
+    gh
+    golang
+    libasound2-dev # Necessary for some sound golang packages
+    libudev-dev    # Necessary for some sound golang packages
+    ranger
+    httpie
+    wireguard
+)
 
-echo -e "$YELLOW Installing ranger...$RESET"
-sudo apt install ranger -y
-echo "set preview_images true" >> ~/.config/ranger/rc.conf
+for app in "${apt_apps[@]}"; do
+    echo -e "$YELLOW Installing $app...$RESET"
+    sudo apt install "$app" -y
+done
+
+# Configure ranger
+echo "set preview_images true" >>~/.config/ranger/rc.conf
 
 ### Install `nvm`
 echo -e "$YELLOW Installing nvm...$RESET"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
 ### Add modularized installation scripts
-echo -e "$YELLOW Installing oh-my-zsh...$RESET"
-. $DOT_FILES_DIR/installation-scripts/install-oh-my-zsh.sh
-echo -e "$YELLOW Installing kitty...$RESET"
-. $DOT_FILES_DIR/installation-scripts/install-kitty.sh
-echo -e "$YELLOW Installing my-go-cli...$RESET"
-. $DOT_FILES_DIR/installation-scripts/install-my-go-cli.sh
-echo -e "$YELLOW Installing vscode...$RESET"
-. $DOT_FILES_DIR/installation-scripts/install-vscode.sh
+modularized_installs=(
+    oh-my-zsh
+    kitty
+    my-go-cli
+    vscode
+)
+
+for app in "${modularized_installs[@]}"; do
+    echo -e "$YELLOW Installing $app...$RESET"
+    . $DOT_FILES_DIR/installation-scripts/install-$app.sh
+done
 
 ### Prompt for flatpak installations
 
@@ -42,9 +49,15 @@ while true; do
     read answer
 
     case $answer in
-        [Yy]* ) x-terminal-emulator --hold --detach zsh -c ". $DOT_FILES_DIR/installation-scripts/install-flatpak-apps.sh"; break;;
-        [Nn]* ) echo "Finished all the setup"; break;;
-        * ) echo "Please answer yes or no.";;
+    [Yy]*)
+        x-terminal-emulator --hold --detach zsh -c ". $DOT_FILES_DIR/installation-scripts/install-flatpak-apps.sh"
+        break
+        ;;
+    [Nn]*)
+        echo "Finished all the setup"
+        break
+        ;;
+    *) echo "Please answer yes or no." ;;
     esac
 done
 
