e86f6cb1f75b1deba1b72cbfe16bbfd042fb6291
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-10-16 00:51:18 -0300

    feat: automate sensible info example copy paste

--- installation-scripts/install-my-go-cli.sh
+++ installation-scripts/install-my-go-cli.sh
@@ -4,4 +4,4 @@ myGoCliPath=$HOME/Documents/GitHub/my-go-cli
 
 git clone https://github.com/luisotaviodesimone/my-go-cli.git $myGoCliPath
 
-sh -c "$(cd $myGoCliPath && go build -o main ./cmd/main.go)" & wait
+sh -c "$(cd $myGoCliPath && go build -o main ./cmd/main.go && cp $myGoCliPath/sensible-info.example.json sensible-info.json)" & wait
