6c8cd46c13fcc2949c66daac969d0d1de1b76d74
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-11-03 16:31:32 -0300

    feat: organize nvm configuration

--- configs/config-completions.sh
+++ configs/config-completions.sh
@@ -1,2 +1,3 @@
 source <(kubectl completion zsh)
 source <(helm completion zsh)
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
 export KITTY_CONFIG_DIRECTORY="$DOT_FILES_DIR/kitty"
+export NVM_DIR="$HOME/.nvm"
 . $DOT_FILES_DIR/configs/config-kubectl.sh
