f440a15b02d95ad92dd90225e4a0648c71239a03
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-11-03 16:55:39 -0300

    fix: config ssh

--- configs/config-ssh.sh
+++ configs/config-ssh.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/bash
+
 # Use xterm as default shell in other hosts
-echo 'Host * \n  Setenv TERM="xterm"' >> ~/.ssh/config
+if ! [[ $(grep -qs "Host *" "$HOME/.ssh/config") ]]; then
+cat > $HOME/.ssh/config <<- EOM
+Host *
+  Setenv TERM="xterm"
+
+EOM
+else
+echo "Já existem configurações para o 'Host *'"
+fi
