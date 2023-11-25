68296da5c0f28413f7deb292d018cc9623f35a2f
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-11-25 16:04:50 -0300

    feat: update kitty config and fix ssh config

--- configs/config-ssh.sh
+++ configs/config-ssh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 # Use xterm as default shell in other hosts
-if ! [[ $(grep -qs "Host *" "$HOME/.ssh/config") ]]; then
-cat > $HOME/.ssh/config <<- EOM
+if ! [[ $(grep -s "Setenv TERM=" "$HOME/.ssh/config") ]]; then
+cat >> $HOME/.ssh/config <<- EOM
+
 Host *
   Setenv TERM="xterm"
-
 EOM
 else
 echo "Já existem configurações para o 'Host *'"
