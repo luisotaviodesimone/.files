43660900b0613e611b148bb3be1d0a4a4362f420
luisotaviodesimone <luisotaviodesimone@gmail.com>
2023-12-19 12:05:48 -0300

    fix: config k8s plugin

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@ export KITTY_CONFIG_DIRECTORY="$DOT_FILES_DIR/kitty"
 export NVM_DIR="$HOME/.nvm"
 export PATH="$HOME/.local/bin:$PATH"
 . $DOT_FILES_DIR/configs/config-kubectl.sh
+export RPROMPT='$(kube_ps1)'
