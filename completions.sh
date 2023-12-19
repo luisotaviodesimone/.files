431534ceda43e0148af93b51b03d6cc79866de26
luisotaviodesimone <luisotaviodesimone@gmail.com>
2023-12-19 12:17:29 -0300

    fix: clean .zshrc file

--- completions.sh
+++ completions.sh
@@ -1,3 +1,5 @@
 source <(kubectl completion zsh)
 source <(helm completion zsh)
+source $ZSH/oh-my-zsh.sh
+
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- env.sh
+++ env.sh
@@ -4,3 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 export PATH="$HOME/.local/bin:$PATH"
 . $DOT_FILES_DIR/configs/config-kubectl.sh
 export RPROMPT='$(kube_ps1)'
+export PATH=$PATH:/usr/local/go/bin
+export ZSH="$HOME/.oh-my-zsh"
+
