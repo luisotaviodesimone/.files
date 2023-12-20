cb4cea5fe0c0830fecf606a44d708f543958e55b
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-12-20 18:56:02 -0300

    feat: fix scripts agreement missing

--- installation-scripts/install-docker.sh
+++ installation-scripts/install-docker.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Make possible to user docker without sudo
 if getent group docker | grep -q "$USER"; then
@@ -22,5 +22,4 @@ echo -e "$RED User '$(whoami)' is already in group 'docker' $RESET"
 else
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 fi
