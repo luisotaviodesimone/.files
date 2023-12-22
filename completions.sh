6a9f4b317a335ce33c166777e92f685f36efa111
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-12-22 14:06:48 -0300

    refactor: change nvm sourcing and autocompleting script place

--- completions.sh
+++ completions.sh
@@ -3,4 +3,5 @@ source <(helm completion zsh)
 eval "$(starship init zsh)"
 source <(starship completions zsh)
 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
