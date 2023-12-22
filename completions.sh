0916e2bd1636b5507cffbc001f3596df8bfcd32a
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-12-22 15:26:17 -0300

    feat: add fast-syntax-highlighting

--- completions.sh
+++ completions.sh
@@ -9,4 +9,6 @@ source <(starship completions zsh)
 # Plugings sourcing
 source $DOT_FILES_DIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $DOT_FILES_DIR/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $DOT_FILES_DIR/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 source $DOT_FILES_DIR/plugins/z.sh
+source $DOT_FILES_DIR/plugins/git.plugin.zsh
