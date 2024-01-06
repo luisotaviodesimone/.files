595c93707c54208c9683749a3c746c23f900b54d
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-01-05 21:08:39 -0300

    feat: create lsp config and finish customizing vim

---
+++ .config/nvim/lua/luisotaviodesimone/plugins/lsp/mason.lua
@@ -0,0 +1,34 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+
+        mason.setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending= "↺",
+                    package_uninstalled = "✗",
+                }
+            }
+        })
+
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "tsserver",
+                "html",
+                "tailwindcss",
+                "lua_ls",
+                "emmet_ls",
+                "pyright",
+                "gopls",
+                "bashls"
+            },
+            automatic_installation = true,
+        })
+    end
+}
