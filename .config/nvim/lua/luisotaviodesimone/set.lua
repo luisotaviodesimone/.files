b18001959ba9f6486e82bd9261861a81c39d4ddc
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-02-16 20:57:42 -0300

    feat: set persistent undo

--- .config/nvim/lua/luisotaviodesimone/set.lua
+++ .config/nvim/lua/luisotaviodesimone/set.lua
@@ -11,3 +11,23 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
+
+-- Set persistent undo
+vim.opt.undodir = '~/.config/nvim/.undo//'
+vim.opt.undofile = true
+
+-- Append backup files with timestamp
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local extension = "~" .. vim.fn.strftime("%Y-%m-%d-%H%M%S")
+		vim.o.backupext = extension
+	end,
+})
+
+-- Set swap directory
+vim.opt.directory = '~/.config/nvim/.swap//'
+vim.opt.swapfile = true
+
+-- Set backup directory
+vim.opt.backupdir = '~/.config/nvim/.backup//'
+vim.opt.backup = true
