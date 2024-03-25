0c6067dbcd9e208c50f7deeddf444420fcf9298c
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-03-25 16:48:00 -0300

    feat: add kubectl repo update script line

--- installation-scripts/install-kubectl.sh
+++ installation-scripts/install-kubectl.sh
@@ -7,7 +7,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Install kubernetes
-sudo apt install kubectl -y
+sudo apt update && sudo apt install kubectl -y
 
 mkdir -p $HOME/.kube/ $HOME/.kube/configs
 touch $HOME/.kube/config
