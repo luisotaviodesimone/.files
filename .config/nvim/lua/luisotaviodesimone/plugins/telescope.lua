2cf49ed37d7cca06b078e97619a0e400d236830d
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-01 22:17:26 -0300

    refactor: format

--- .config/nvim/lua/luisotaviodesimone/plugins/telescope.lua
+++ .config/nvim/lua/luisotaviodesimone/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     defaults = {
 
@@ -11,14 +12,14 @@ return {
             builtin.find_files({ hidden = true, no_ignore = true })
         end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function() 
+        vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({
                 search = vim.fn.input("Grep > "),
                 additional_args = {
                     "--hidden",
                     '--glob', -- this flag allow you to exclude given files from search
                     '!{**/.git/*,**/node_modules/*,**/package-lock.json,**/yarn.lock}',
-                } 
+                }
             })
         end)
     end,
