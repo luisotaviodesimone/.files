934d0adaab3596dbac8f05c85c1410d8afe6292d
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-07 00:38:10 -0300

    fix: automatic node installation

--- installation-scripts/install-node.sh
+++ installation-scripts/install-node.sh
@@ -2,4 +2,9 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
-$HOME/.nvm/nvm.sh install --lts
+NVM_DIR=$HOME/.nvm
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts
+nvm use --lts
