ce1d65e458250a6a160c1c5863769ed412afbfae
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-12 19:40:17 -0300

    feat: improve lods installation

--- installation-scripts/install-lods.sh
+++ installation-scripts/install-lods.sh
@@ -6,5 +6,5 @@ git clone https://github.com/luisotaviodesimone/my-go-cli.git $myGoCliPath
 
 sh -c "$(cd $myGoCliPath && go build -o $HOME/.local/bin/lods ./cmd/main.go && cp $myGoCliPath/sensible-info.example.json sensible-info.json)" & wait
 
-sed -i s/personal-github-name-here/\$USER/ $myGoCliPath/sensible-info.json
-sed -i s/personal-github-email-here@email.com/\$USER@gmail.com/ $myGoCliPath/sensible-info.json
+sed -i s/personal-github-name-here/$USER/ $myGoCliPath/sensible-info.json
+sed -i s/personal-github-email-here@email.com/$USER@gmail.com/ $myGoCliPath/sensible-info.json
