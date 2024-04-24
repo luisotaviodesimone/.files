0dd024879bdd14bb04b80a775183a159b2121235
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-23 22:56:55 -0300

    feat: format and substitute colorscheme package

--- .config/nvim/lua/luisotaviodesimone/plugins/colorscheme.lua
+++ .config/nvim/lua/luisotaviodesimone/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
   {
-    'maxmx03/dracula.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "binhtran432k/dracula.nvim",
+    name = 'dracula',
+    lazy = false,
+    priority = 1000,
     config = function ()
       local dracula = require 'dracula'
 
