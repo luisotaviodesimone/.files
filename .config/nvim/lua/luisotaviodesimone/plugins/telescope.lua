98ce30c2357ed656a5fd13192291536e20441b39
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-24 13:39:20 -0300

    feat format and tweak telescope

--- .config/nvim/lua/luisotaviodesimone/plugins/telescope.lua
+++ .config/nvim/lua/luisotaviodesimone/plugins/telescope.lua
@@ -1,26 +1,38 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    defaults = {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  defaults = {
 
-    },
-    config = function()
-        local builtin = require('telescope.builtin')
+  },
+  config = function()
+    local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>pf', function()
-            builtin.find_files({ hidden = true, no_ignore = true })
-        end)
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({
-                search = vim.fn.input("Grep > "),
-                additional_args = {
-                    "--hidden",
-                    '--glob', -- this flag allow you to exclude given files from search
-                    '!{**/.git/*,**/node_modules/*,**/package-lock.json,**/yarn.lock}',
-                }
-            })
-        end)
-    end,
+    vim.keymap.set('n', '<C-p>', function()
+      builtin.find_files({
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "--no-ignore-vcs",
+          "-g",
+          "!**/.git/*",
+          "-g",
+          "!**/node_modules/*",
+          "-g", "!**/.repro/*",
+        }
+      })
+    end)
+    vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({
+        search = vim.fn.input("Grep > "),
+        additional_args = {
+          "--hidden",
+          '--glob',
+          '!{**/.git/*,**/node_modules/*,**/package-lock.json,**/yarn.lock}',
+        }
+      })
+    end)
+  end,
 }
