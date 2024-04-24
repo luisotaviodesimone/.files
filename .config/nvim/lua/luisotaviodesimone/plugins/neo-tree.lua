fff8e09afffebc75bd4296feb7dff11434c283fa
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-24 13:41:23 -0300

    fix: add neo-tree

--- .config/nvim/lua/luisotaviodesimone/plugins/neo-tree.lua
+++ .config/nvim/lua/luisotaviodesimone/plugins/neo-tree.lua
@@ -8,8 +8,5 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<C-b>', ':Neotree toggle left<CR>', {})
---    require("neo-tree").setup({
---      close_if_last_window = true,
---    })
   end
 }
