2515401161c09d46f63f5a452e4bdbb36cbc3dd6
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-28 20:25:32 -0300

    feat: flexify kitty theme

--- aliases.sh
+++ aliases.sh
@@ -14,6 +14,7 @@ alias apply="source $HOME/.zshrc"
 alias copy="xclip -selection c"
 alias clc="fc -ln -1 | copy"
 alias instaK8s="k3d cluster create --servers 3 --agents 1 local-cluster && k3d kubeconfig get local-cluster | sed s/k3d-//g > ~/.kube/configs/local-cluster"
+alias kitty-theme="kitty +kitten themes --reload-in=all"
 
 # Kubectl shortcuts
 # alias kok="kubectl get pods --all-namespaces -o wide | awk -F " *|/" '($3!=$4 || $5!="Running") && $5!="Completed" {print $0}'"
