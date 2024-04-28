2a81f67d1e36371fc7ccc493ebad365d7f5f098f
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-04-28 18:26:13 -0300

    feat: add verification to 'yq' installation

--- installation-scripts/install-yq.sh
+++ installation-scripts/install-yq.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
-sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
-sudo chmod +x /usr/bin/yq
+if [[ ! -f /usr/bin/yq ]]; then
+  sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
+  sudo chmod +x /usr/bin/yq
+else
+  echo -e "$RED\nThe 'yq' tool is already installed!$RESET"
+fi
