ac035be9f1ec6c0cf99cc44c57280c2fa7231f7d
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-05-22 23:43:58 -0300

    feat: finish configuring tmux file

--- .config/nvim/lua/luisotaviodesimone/plugins/harpoon.lua
+++ .config/nvim/lua/luisotaviodesimone/plugins/harpoon.lua
@@ -8,8 +8,8 @@ return {
         local ui = require("harpoon.ui")
         local mark = require("harpoon.mark")
 
-        vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+--        vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+--        vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
 
         vim.keymap.set("n", "<leader>a", function() mark.add_file() end)
         vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end)
