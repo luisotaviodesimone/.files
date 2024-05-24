7c9db44470bf52cea85572d97bfaef03a3e3a0e2
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-05-24 10:24:23 -0300

    feat: create debian i3 script installation

---
+++ installation-scripts/install-i3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3
