b0289023ee34c448c8ea63f0bc8f3a6415bb8552
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-05-28 23:29:36 -0300

    feat: update terraform installation

--- installation-scripts/install-terraform.sh
+++ installation-scripts/install-terraform.sh
@@ -3,7 +3,7 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 
 # Add HashiCorp GPG key
 wget -O- https://apt.releases.hashicorp.com/gpg | \
-  gpg --dearmor | \
+  gpg --batch --yes --dearmor | \
   sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 
 # Add HashiCorp repository
@@ -13,4 +13,4 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 
 
 # Update and install Terraform
-sudo apt update && sudo apt install terraform
+sudo apt update && sudo apt install terraform -y
