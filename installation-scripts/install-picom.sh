b4f6436f2d2759a9f6f89de79a44b855387beefa
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-05-30 23:11:50 -0300

    feat: create picom installation script

---
+++ installation-scripts/install-picom.sh
@@ -0,0 +1,10 @@
+# clone and cd into picom repo
+git clone git@github.com:yshui/picom.git
+cd picom
+
+# install dependencies
+sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev -y
+
+# build and install picom
+meson setup --buildtype=release build
+sudo ninja -C build install
