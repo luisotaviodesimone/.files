6b188e67c8009e124e5b1d8ab681cac734b81f59
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-03 18:53:34 -0300

    feat: remove packer from nvim installation

--- installation-scripts/install-nvim.sh
+++ installation-scripts/install-nvim.sh
@@ -2,6 +2,3 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage $HOME/.local/bin/nvim
 sudo ln -s $HOME/.local/bin/nvim /bin/nvim
-
-mkdir -p $HOME/.local/share/nvim/site/pack/packer/start
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
