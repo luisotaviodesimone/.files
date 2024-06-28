d138ab782c68de658aa6d467c551fd7efb8a2d7a
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-06-27 22:03:05 -0300

    feat: confiugure default dark theme

--- configs/config-config-files.sh
+++ configs/config-config-files.sh
@@ -2,3 +2,6 @@ mkdir -p ~/.config
 
 ln -sf $DOT_FILES_DIR/.config/* ~/.config/
 
+# Configure default dark theme
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+echo "[Settings]\ngtk-application-prefer-dark-theme = true" >> ~/.config/gtk-3.0/settings.ini
