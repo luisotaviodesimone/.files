786cf29c8c7a6eeb1da839e3ac41e45f2f1e40ba
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-09-06 18:51:27 -0300

    feat: update env file

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
 export KITTY_CONFIG_DIRECTORY="$DOT_FILES_DIR/kitty"
 export NVM_DIR="$HOME/.nvm"
-export PATH="$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$PATH"
+export PATH="$HOME/.platformio/penv/bin:/$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$PATH"
 . $DOT_FILES_DIR/configs/config-kubectl.sh
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 export KUBE_EDITOR="/home/luisotaviodesimone/.local/bin/nvim"
