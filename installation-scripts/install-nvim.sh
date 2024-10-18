c098083313559a88220ffa73d04a732bb1b9538c
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-18 15:26:59 -0300

    feat: force nvim update

--- installation-scripts/install-nvim.sh
+++ installation-scripts/install-nvim.sh
@@ -1,4 +1,4 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage $HOME/.local/bin/nvim
-sudo ln -s $HOME/.local/bin/nvim /bin/nvim
+sudo ln -sf $HOME/.local/bin/nvim /bin/nvim
