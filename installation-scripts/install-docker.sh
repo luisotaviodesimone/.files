1c42df15f573c69c501b5ce73ae73a746debc2a6
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-19 18:59:23 -0300

    fix: docker installation script

--- installation-scripts/install-docker.sh
+++ installation-scripts/install-docker.sh
@@ -9,7 +9,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID) \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
