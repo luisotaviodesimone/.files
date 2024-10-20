8c3603ad9df063ef0af0e04d63652ce10139bb7a
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-19 23:20:47 -0300

    feat: ensure directory

--- installation-scripts/install-nvim.sh
+++ installation-scripts/install-nvim.sh
@@ -1,4 +1,5 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
+mkdir -p $HOME/.local/bin/nvim
 mv nvim.appimage $HOME/.local/bin/nvim
 sudo ln -sf $HOME/.local/bin/nvim /bin/nvim
