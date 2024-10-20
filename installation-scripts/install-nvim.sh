6c74a66c5630bee861a2ff0a33a504488ada3301
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-19 23:22:10 -0300

    fix: dir path

--- installation-scripts/install-nvim.sh
+++ installation-scripts/install-nvim.sh
@@ -1,5 +1,5 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-mkdir -p $HOME/.local/bin/nvim
+mkdir -p $HOME/.local/bin
 mv nvim.appimage $HOME/.local/bin/nvim
 sudo ln -sf $HOME/.local/bin/nvim /bin/nvim
