3eb2e8281a5e8bb8367d3f6dbc0ac55dadf4369d
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-28 12:11:06 -0300

    feat: update go version

--- installation-scripts/install-go.sh
+++ installation-scripts/install-go.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
 
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
 
 sudo apt install libasound2-dev libudev-dev -y
 
-rm go1.22.2.linux-amd64.tar.gz
+rm go1.23.2.linux-amd64.tar.gz
