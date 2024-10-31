50fcde9aa3f3764be94203c32fdd1f6cccdd2889
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-10-31 20:01:41 -0300

    feat: check for starship script installations

--- installation-scripts/install-starship.sh
+++ installation-scripts/install-starship.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/bash
 
-# Set zsh as default shell
-if [[ $SHELL == *"zsh"* ]]; then
-    echo -e "$RED 'zsh' is already the default shell $RESET"
+# Check if zsh is installed and is set to default shell
+
+if [[ -f /bin/zsh ]]; then
+    if [[ $SHELL == *"zsh"* ]]; then
+        echo -e "$RED 'zsh' is already the default shell $RESET"
+    else
+        echo -e "$GREEN Setting zsh as default shell"
+        sudo chsh -s /bin/zsh $USER
+    fi
 else
-    echo "Setting zsh as default shell"
-    sudo chsh -s $(which zsh) $USER
+    echo -e "$RED 'zsh' is not installed $RESET"
 fi
 
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+if (which starship > /dev/null); then
+    echo -e "$RED 'starship' is already installed"
+else
+    curl -sS https://starship.rs/install.sh | sh -s -- -y
+fi
+
 
 # Link .zshrc to home directory
+if [[ ! -z $DOT_FILES_DIR ]]; then
+    export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
+fi
+
 ln -sf $DOT_FILES_DIR/.zshrc $HOME/.zshrc
