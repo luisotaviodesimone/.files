41b2b5b57fb8c9a8f9863e62ce40683b91f4e5f1
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-11-07 12:15:35 -0300

    feat: update startship installation script

--- installation-scripts/install-starship.sh
+++ installation-scripts/install-starship.sh
@@ -6,19 +6,18 @@ if [[ -f /bin/zsh ]]; then
     if [[ $SHELL == *"zsh"* ]]; then
         echo -e "$RED 'zsh' is already the default shell $RESET"
     else
-        echo -e "$GREEN Setting zsh as default shell"
+        echo -e "$GREEN\bSetting zsh as default shell"
         sudo chsh -s /bin/zsh $USER
+    fi
+
+    if (which starship > /dev/null); then
+        echo -e "$RED 'starship' is already installed"
+    else
+        curl -sS https://starship.rs/install.sh | sh -s -- -y
     fi
 else
     echo -e "$RED 'zsh' is not installed $RESET"
 fi
-
-if (which starship > /dev/null); then
-    echo -e "$RED 'starship' is already installed"
-else
-    curl -sS https://starship.rs/install.sh | sh -s -- -y
-fi
-
 
 # Link .zshrc to home directory
 if [[ -z $DOT_FILES_DIR ]]; then
