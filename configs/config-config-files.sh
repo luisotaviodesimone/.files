571df8a8f391661f4e86c3e0e6022981498c3d3e
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-11-08 01:06:28 -0300

    feat: set default gtk dark theme

--- configs/config-config-files.sh
+++ configs/config-config-files.sh
@@ -5,3 +5,4 @@ ln -sf $DOT_FILES_DIR/.config/* ~/.config/
 # Configure default dark theme
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 echo "[Settings]\ngtk-application-prefer-dark-theme = true" >> ~/.config/gtk-3.0/settings.ini
+echo "export GTK_THEME=Adwaita:dark" >> ~/.profile
