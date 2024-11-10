0d74427305198e1878805fd0115a5aaf416259c3
luisotaviodesimone <luisotaviodesimone@gmail.com>
2024-11-10 00:15:38 -0300

    feat: make KUBE_EDITOR dynamic for nvim path

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
+export PATH="$HOME/.platformio/penv/bin:/$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$HOME/.krew/bin:$PATH"
 export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
 export KITTY_CONFIG_DIRECTORY="$DOT_FILES_DIR/kitty"
 export NVM_DIR="$HOME/.nvm"
-export PATH="$HOME/.platformio/penv/bin:/$HOME/.local/bin:/usr/local/go/bin:$HOME/go/bin:$HOME/.krew/bin:$PATH"
 . $DOT_FILES_DIR/configs/config-kubectl.sh
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
-export KUBE_EDITOR="/home/luisotaviodesimone/.local/bin/nvim"
+export KUBE_EDITOR=$(which nvim)
 export SDKMAN_DIR="$HOME/.sdkman"
 export CPLUS_INCLUDE_PATH=/usr/include/c++/11:/usr/include/x86_64-linux-gnu/c++/11
 export PYENV_ROOT="$HOME/.pyenv"
