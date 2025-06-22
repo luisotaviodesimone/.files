a789832654e6df5ed3ea5f0ff132c0c734e05de9
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-06-21 23:24:50 -0300

    feat: add exa alias

--- aliases.sh
+++ aliases.sh
@@ -45,10 +45,10 @@ alias gst="git status"
 
 # Directory Aliases
 alias ..="cd .."
-alias ls="ls --color=auto"
+alias ls="exa --color=auto"
 alias l="ls -1"
 alias ll="ls -l"
-alias la="ls -1A"
+alias la="ls -1a"
 alias lla="ls -la"
 
 # Bat alias with fallback in case batcat is called only bat
