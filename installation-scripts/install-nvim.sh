7eb3a4fafa57a41a60c4906deec5bb76f2c66d5a
luisotaviodesimone <luisotaviodesimone@gmail.com>
2025-12-17 01:29:08 -0300

    feat: install latest neovim

--- installation-scripts/install-nvim.sh
+++ installation-scripts/install-nvim.sh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/neovim/neovim-releases/releases/download/v0.10.4/nvim-linux-x86_64.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
 mkdir -p $HOME/.local/bin
 mv nvim-linux-x86_64.appimage $HOME/.local/bin/nvim
