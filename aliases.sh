4f15051227980669a45f32d3843fbcd920c61860
luisotaviodesimone <luisotaviodesimone@gmail.com>
2026-01-11 04:41:54 -0300

    feat: add swayconfig alias

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,7 @@ alias ssconfig="nvim $HOME/.config/starship/starship.toml"
 alias alctyconfig="nvim $HOME/.config/alacritty/alacritty.toml"
 alias tmuxconfig="nvim $HOME/.config/tmux/tmux.conf"
 alias i3config="nvim $HOME/.config/i3/config"
+alias swayconfig="nvim $HOME/.config/sway/config"
 
 # CLI shortcuts
 
